3624e365246ccced645a84481a3bd4e29190dab5
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-03-11 10:37:32 +0700

    input

--- if_else.cpp
+++ if_else.cpp
@@ -3,4 +3,9 @@ using namespace std;
 
 int main() {
     float bmi, berat, tinggi;
+
+    cout << "Berapa berat badan kamu (kg)? ";
+    cin >> berat;
+    cout << "Berapa tinggi badan kamu (meter, contoh 1.7)? ";
+   
 }
